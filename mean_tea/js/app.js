466bbb49f8be4c2012db99a0213c2928d23447c0
Rachel Parri <erachelparri@gmail.com>
2016-02-26 09:56:17 -0700

    changed index.html to list.html

--- mean_tea/js/app.js
+++ mean_tea/js/app.js
@@ -3,7 +3,7 @@ var app = angular.module('mean_teaApp', ['ngRoute'])
 		app.config(function($routeProvider) {
 			$routeProvider
 				.when('/', {
-					templateUrl: '../partials/index.html',
+					templateUrl: '../partials/list.html',
 					controller: 'MainController'
 				})
 				.otherwise({redirectTo : '/'})
