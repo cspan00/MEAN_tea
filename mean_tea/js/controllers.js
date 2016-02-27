d2438c84caa9b72bc45fbfe39456322b3a03fdc9
Rachel Parri <erachelparri@gmail.com>
2016-02-26 23:30:14 -0700

    changed true/false in stock to yes/no

--- mean_tea/js/controllers.js
+++ mean_tea/js/controllers.js
@@ -2,26 +2,6 @@ app.controller('MainController', ['$scope', 'Teas', function($scope, Teas){
       $scope.teas = Teas.teas;
       $scope.add = Teas.add;
       $scope.cart = Teas.cart;
-      // $scope.cart.length = Teas.cart.length
-      if ($scope.cart.length === 0) {
-        return "empty"
-      }
-      if ($scope.cart.length > 0) {
-        return $scope.cart.length
-      }
-      // $scope.cart.length = Teas.cart.length;
-      // $scope.cartLength = cartLength();
-
-    //   function cartLength() {
-    //     var cartLength = $scope.cart.length
-    //     if (cartLength === 0) {
-    //       return "Empty";
-    //     if (cartLength > 0) {
-    //       return cartLength;
-    //     }
-    //   }
-    // }
-
     }]);
 
 app.controller('CartController', ['$scope', 'Teas', function($scope, Teas){
