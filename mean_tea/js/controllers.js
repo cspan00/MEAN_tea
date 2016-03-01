df145872e4ff7bc199693776ba1c2490151f294e
Rachel Parri <erachelparri@gmail.com>
2016-03-01 15:38:05 -0700

    sort by wroks as it should

--- mean_tea/js/controllers.js
+++ mean_tea/js/controllers.js
@@ -2,6 +2,8 @@ app.controller('MainController', ['$scope', 'Teas', function($scope, Teas){
       $scope.teas = Teas.teas;
       $scope.add = Teas.add;
       $scope.cart = Teas.cart;
+      $scope.quantity = '1';
+
     }]);
 
 app.controller('CartController', ['$scope', 'Teas', function($scope, Teas){
